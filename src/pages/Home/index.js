d1e89b030eb08015cc59e2ad48745bed793c6570
Mickael Araujo <araujomickael18@gmail.com>
2020-05-25 13:12:44 -0300

    view: Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,111 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { Image, Animated } from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
+
+import { Container } from '../../styles/global'
+import {
+    Welcome, TextContainer, WelcomeText,
+    Name, Balance, BalanceText, BalanceContainer,
+    BalanceValue, HideButton, GreenTextContainer,
+    FilterDateButton, GreenText, ActivityContainer,
+    ActivityStatus, StatusContainer, StatusText,
+    StatusValue, ActivityCard, MinimizeButton
+} from './styles'
+
+import DrawerToggler from '../../components/DrawerToggler'
+import logo from '../../assets/Logo1.png'
 
 export default function Home() {
+    const [hide, setHide] = useState(false)
+    const [ opacity ] = useState(new Animated.Value(1))
+
+    const AnimatedText = Animated.createAnimatedComponent(BalanceValue)
+
+    function handleHide() {
+        setHide(!hide)
+
+        if (!hide) {
+            Animated.timing(opacity, {
+                toValue: 0,
+                duration: 400
+            }).start()
+        } else {
+            Animated.timing(opacity, {
+                toValue: 1,
+                duration: 400
+            }).start()
+        }
+    }
+
     return (
-        <View style={{
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center'
-        }}>
-            <Text>Home</Text>
-        </View>
+        <Container>
+            <DrawerToggler />
+
+            <Welcome>
+                <Image
+                source={logo}
+                />
+
+                <TextContainer>
+                    <WelcomeText>
+                        Bem Vindo,
+                    </WelcomeText>
+
+                    <Name>Mickael!</Name>
+                </TextContainer>
+            </Welcome>
+
+            <Balance>
+                <BalanceText>saldo:</BalanceText>
+
+                <BalanceContainer>
+                    <AnimatedText
+                    style={{ opacity: opacity }}
+                    >
+                        R$ 109,855,60
+                    </AnimatedText>
+
+                    <HideButton
+                    onPress={handleHide}
+                    >
+                        { hide ? (
+                        <Icon name='eye' size={26} color='#000000' />
+                        ) : (
+                        <Icon name='eye-off' size={26} color='#000000' />
+                        ) }
+                    </HideButton>
+                </BalanceContainer>
+            </Balance>
+
+            <GreenTextContainer>
+                <FilterDateButton>
+                    <Icon name='calendar' size={32} color='#0BB24E' />
+                </FilterDateButton>
+
+                <GreenText>últimas movimentações</GreenText>
+            </GreenTextContainer>
+
+            <ActivityContainer>
+                <ActivityStatus>
+                    <StatusContainer>
+                        <StatusText>total gastos:</StatusText>
+                        
+                        <StatusValue value='low'>R$ 865,50</StatusValue>
+                    </StatusContainer>
+
+                    <StatusContainer>
+                        <StatusText>total ganhos:</StatusText>
+
+                        <StatusValue>R$ 3267,27</StatusValue>
+                    </StatusContainer>
+                </ActivityStatus>
+
+                <ActivityCard>
+                    <MinimizeButton>
+                        <Icon name='minimize-2' size={26} color='#000000' />
+                    </MinimizeButton>
+                </ActivityCard>
+            </ActivityContainer>
+        </Container>
     )
 }
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,126 @@
+import styled from 'styled-components/native'
+import { getStatusBarHeight } from 'react-native-status-bar-height'
+
+export const Welcome = styled.View`
+    margin-top: ${45 + getStatusBarHeight()}px;
+    padding: 10px;
+    width: 100%;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+`
+
+export const TextContainer = styled.View`
+    margin-left: 35px;
+    margin-top: 10px;
+`
+
+export const WelcomeText = styled.Text`
+    font-family: 'Roboto-Thin';
+    font-size: 36px;
+    letter-spacing: 1px;
+`
+
+export const Name = styled.Text`
+    font-size: 40px;
+    font-family: 'Roboto-BoldItalic';
+    letter-spacing: 1px;
+    color: #0BB24E;
+`
+
+export const Balance = styled.View`
+    width: 100%;
+    padding: 15px 25px;
+`
+
+export const BalanceText = styled.Text`
+    font-family: 'Roboto-LightItalic';
+    font-size: 24px;
+    letter-spacing: 1px;
+`
+
+export const BalanceContainer = styled.View`
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const BalanceValue = styled.Text`
+    font-family: 'Roboto-Bold';
+    font-size: 36px;
+    letter-spacing: 1px;
+`
+
+export const HideButton = styled.TouchableOpacity`
+    justify-content: center;
+    align-items: center;
+`
+
+export const GreenTextContainer = styled.View`
+    width: 100%;
+    padding-left: 25px;
+    padding-bottom: 10px;
+    flex-direction: row;
+    align-items: baseline;
+`
+
+export const FilterDateButton = styled.TouchableOpacity`
+    justify-content: center;
+    align-items: center;
+`
+
+export const GreenText = styled.Text`
+    margin-left: 5px;
+    font-size: 18px;
+    font-family: 'Roboto-Bold';
+    color: #0BB24E;
+`
+
+export const ActivityContainer = styled.View`
+    width: 100%;
+    flex: 1;
+    align-items: center;
+    position: relative;
+`
+
+export const ActivityStatus = styled.View`
+    width: 100%;
+    padding-top: 10px;
+    padding-left: 25px;
+    z-index: 3;
+`
+
+export const StatusContainer = styled.View`
+    margin-top: 15px;
+`
+
+export const StatusText = styled.Text`
+    font-family: 'Roboto-Light';
+    font-size: 24px;
+`
+
+export const StatusValue = styled.Text`
+    font-family: 'Roboto-BoldItalic';
+    font-size: 40px;
+    color: ${props => props.value === 'low' ? '#E71A1A' : '#01933B'}
+`
+
+export const ActivityCard = styled.View`
+    position: absolute;
+    bottom: 0; 
+    flex: 1;
+    background: #fff;
+    width: 95%;
+    height: 100%;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    z-index: 9;
+    /* elevation: 15; */
+`
+
+export const MinimizeButton = styled.TouchableOpacity`
+    align-self: flex-end;
+    padding: 15px 15px;
+`
