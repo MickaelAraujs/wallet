50e1a6d976e2011963533608f726a7e1efbd2bca
Mickael Araujo <araujomickael18@gmail.com>
2020-05-25 08:51:31 -0300

    view: profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,14 +1,52 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useContext } from 'react'
+import { Image } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+
+import { Container, ButtonGreen, ButtonText } from '../../styles/global'
+import {
+    ProfileContainer,
+    Name,
+    Email,
+    Actions,
+    SignOutButton
+} from './styles'
+
+import DrawerToggler from '../../components/DrawerToggler'
+import { AuthContext } from '../../contexts/auth'
+
+import avatar from '../../assets/AVATAR.png'
 
 export default function Profile() {
+    const navigation = useNavigation()
+
+    const { signOut } = useContext(AuthContext)
+
     return (
-        <View style={{
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center'
-        }}>
-            <Text>Profile</Text>
-        </View>
+        <Container>
+            <DrawerToggler />
+
+            <ProfileContainer>
+                <Image
+                source={avatar}
+                />
+
+                <Name>MICKAEL ARAUJO</Name>
+                <Email>mickaelaraujo@fakeemail.com</Email>
+            </ProfileContainer>
+
+            <Actions>
+                <ButtonGreen
+                onPress={() => navigation.navigate('NewRegister')}
+                >
+                    <ButtonText>NOVO REGISTRO</ButtonText>
+                </ButtonGreen>
+
+                <SignOutButton
+                onPress={() => signOut()}
+                >
+                    <ButtonText>SAIR DA CONTA</ButtonText>
+                </SignOutButton>
+            </Actions>
+        </Container>
     )
 }
---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components/native'
+
+export const ProfileContainer = styled.View`
+    margin-top: 45px;
+    justify-content: center;
+    align-items: center;
+`
+
+export const Name = styled.Text`
+    font-family: 'Ranchers-Regular';
+    font-size: 24px;
+    margin-top: 15px;
+    letter-spacing: 1px;
+`
+
+export const Email = styled.Text`
+    font-family: 'Roboto-LightItalic';
+    font-size: 18px;
+`
+
+export const Actions = styled.View`
+    margin-top: 45px;
+`
+
+export const SignOutButton = styled.TouchableOpacity`
+    width: 288px;
+    height: 53px;
+    background-color: #E71A1A;
+    border-radius: 5px;
+    justify-content: center;
+    align-items: center;
+    margin-top: 15px;
+    align-self: center;
+`
