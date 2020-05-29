95b3695b3ab9f4bd9c8ac9966ee16286f1b0ab1f
Mickael Araujo <araujomickael18@gmail.com>
2020-05-29 13:38:09 -0300

    feat: add new register to database

--- src/pages/NewRegister/index.js
+++ src/pages/NewRegister/index.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { 
     KeyboardAvoidingView,
     Keyboard ,
-    TouchableWithoutFeedback
+    TouchableWithoutFeedback,
+    Alert
 } from 'react-native'
+
+import firebase from '../../services/firebase'
+import { format } from 'date-fns'
+import { useNavigation } from '@react-navigation/native'
 
 import {
     Container,
@@ -17,6 +22,8 @@ import {
 
 import { RegisterContainer, Title } from './styles'
 
+import { AuthContext } from '../../contexts/auth'
+
 import DrawerToggler from '../../components/DrawerToggler'
 import Dropdown from '../../components/Dropdown'
 
@@ -24,6 +31,66 @@ export default function NewRegister() {
     const [value, setValue] = useState('')
     const [type, setType] = useState(null)
     const [comment, setComment] = useState('')
+
+    const { user } = useContext(AuthContext)
+
+    const navigation = useNavigation()
+
+    function handleSubmit() {
+        Keyboard.dismiss()
+
+        if (value === '' || type === null) {
+            Alert.alert('Operação Inválida', 'preencha os campos necessários!')
+            return
+        }
+
+        Alert.alert(
+            'confirmando registro',
+            `verifique se os dados estão corretos (valor: ${value} - tipo: ${type})`,
+            [
+                {
+                    text: 'cancelar',
+                    style: 'cancel'
+                },
+
+                {
+                    text: 'confirmar',
+                    onPress: () => handleNewRegister(),
+                }
+            ]
+        )
+    }
+
+    async function handleNewRegister() {
+        const key = await firebase.database().ref('history').push().key
+
+        await firebase.database().ref('history').child(user.uid).child(key).set({
+            value: parseFloat(value),
+            type,
+            date: format(new Date(), 'dd/MM/yyyy'),
+            comment
+        })
+
+        const userRef = await firebase.database().ref('users').child(user.uid)
+        
+        await userRef.once('value', snapshot => {
+            let balance = parseFloat(snapshot.val().balance)
+
+            if (type === 'ganho') {
+                balance += parseFloat(value)
+            } else {
+                balance -= parseFloat(value)
+            }
+
+            userRef.child('balance').set(balance)
+        })
+
+        setValue('')
+        setType(null)
+        setComment('')
+
+        navigation.navigate('Home')
+    }
     
     return (
         <TouchableWithoutFeedback
@@ -65,6 +132,7 @@ export default function NewRegister() {
 
                             <ButtonGreen
                             size={300}
+                            onPress={handleSubmit}
                             >
                                 <ButtonText>REGISTRAR</ButtonText>
                             </ButtonGreen>
